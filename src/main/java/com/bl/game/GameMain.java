93f800dab3826d9a0c20b1bbf8c4a21e9926ab75
julinbestina <julin.bestina@yahoo.com>
2022-06-19 16:55:29 +0530

    Added code to check whether next move is win, draw or select next location

--- src/main/java/com/bl/game/GameMain.java
+++ src/main/java/com/bl/game/GameMain.java
@@ -1,17 +1,17 @@
 package com.bl.game;
 
-import java.util.Random;
+import java.util.Scanner;
 
 public class GameMain {
     public static void main(String[] args) {
 
         TicTacToeGame game = new TicTacToeGame();
 
-        game.initializeBoard();
-        game.showBoard();
-        game.selectInput();
-        game.selectStartingPlayer();
-        game.selectBoardLocation();
+            game.initializeBoard();
+            game.showBoard();
+            game.selectInput();
+            char turn = game.selectStartingPlayer();
+            game.selectBoardLocation(turn);
 
     }
 }
--- src/main/java/com/bl/game/TicTacToeGame.java
+++ src/main/java/com/bl/game/TicTacToeGame.java
@@ -2,10 +2,13 @@ package com.bl.game;
 
 import java.util.Scanner;
 
+import static java.lang.System.exit;
+
 public class TicTacToeGame {
 
     char[] board = new char[10];
     char userInput, computerInput, player;
+    int count = 1;
     Scanner sc = new Scanner(System.in);
 
     public void initializeBoard() {
@@ -34,39 +37,76 @@ public class TicTacToeGame {
         System.out.println("User input: " + userInput + ", " + "Computer input: " + computerInput);
     }
 
+    public void selectBoardLocation(char turn) {
 
-    public void selectBoardLocation() {
-
-        System.out.println("Select Location in the board to insert input from 1-9: ");
+        System.out.println("Select Location in the board to insert input from 1-9 for " + turn + ": ");
         int userLocation = sc.nextInt();
-        selectBoardLocationRec(userLocation);
+        selectBoardLocationRec(userLocation, turn);
     }
 
-
-    private void selectBoardLocationRec(int userLocation) {
+    private void selectBoardLocationRec(int userLocation, char turn) {
 
         if (userLocation > board.length - 1) {
             System.out.println("Enter correct location");
-            selectBoardLocation();
+            selectBoardLocation(turn);
         }
 
-        for (int i = 0; i < board.length; i++) {
+        for (int i = 1; i < board.length; i++) {
             if (userLocation == i && board[i] == ' ') {
-                board[i] = userInput;
+                board[i] = turn;
                 showBoard();
-                break;
+                checkNextMove(turn);
             } else if (userLocation == i && board[i] != ' ') {
                 System.out.println("Location already used");
-                selectBoardLocation();
+                selectBoardLocation(turn);
             }
         }
     }
 
-    public void selectStartingPlayer() {
+    public char selectStartingPlayer() {
 
         int coinFlip = (int) ((Math.random() * 10) % 2);
-        player = coinFlip == 1 ? userInput : computerInput;
+        player = (coinFlip == 1) ? userInput : computerInput;
         System.out.println("Starting Player is " + player);
+        return player;
+    }
+
+    public void checkNextMove(char turn) {
+        for (int i = 1; i < 8; i = i + 3) {
+            if (board[i] == turn && board[i + 1] == turn && board[i + 2] == turn) {
+                System.out.println("winner is " + turn);
+                exit(0);
+            }
+        }
+
+        for (int i = 1; i < 4; i = i + 1) {
+            if (board[i] == turn && board[i + 3] == turn && board[i + 6] == turn) {
+                System.out.println("winner is " + turn);
+                exit(0);
+            }
+        }
+
+        if (board[1] == turn && board[5] == turn && board[9] == turn) {
+            System.out.println("winner is " + turn);
+            exit(0);
+        } else if (board[3] == turn && board[5] == turn && board[7] == turn) {
+            System.out.println("winner is " + turn);
+            exit(0);
+        } else {
+            for (int i = 1; i < board.length; i++) {
+                if (board[i] == ' ') {
+                    selectBoardLocation((turn == computerInput) ? userInput : computerInput);
+                    count++;
+                }
+                if (count == 9) {
+                    System.out.println("draw and Thank you");
+                    exit(0);
+                }
+            }
+
+        }
+
     }
 }
+
 
